40c7ec0f425719ce71797d9fa9cee412a0c8d8fe
aligharouni <aligharouni@gmail.com>
2020-12-10 21:52:22 -0500

    plot single panle

--- codes/sir_plot.R
+++ codes/sir_plot.R
@@ -2,6 +2,9 @@
 source("SIRfunctions.R")
 source("params")
 
+# #################################
+# Making Dataframe part
+# #################################
 # R0 contour plots
 n_out <- 5 ## facets (rows/cols)
 n_in <- 41 ## grid N within facets
@@ -10,11 +13,20 @@ n_in <- 41 ## grid N within facets
 W_S_random <- 1
 W_S_targeted <- 0.3 
 
+# make dataframe
 eval_R0(params = update(params,W_S=W_S_random),filename="random_test_df.csv")
 eval_R0(params = update(params,W_S=W_S_targeted),filename="targeted_test_df.csv")
 
+# #################################
+# Load the data
+# #################################
+df_random <- read.csv(file = 'random_test_df.csv')
+df_targeted <- read.csv(file = 'targeted_test_df.csv')
 
-
+# #################################
+# Plotting Part
+# #################################
+# Setting the plots
 ## hacked (not very robustly) to chain label_both() and label_parsed() ...
 label_special <- function (labels, multi_line=FALSE, sep = "== ") {
   value <- ggplot2:::label_value(labels, multi_line = multi_line)
@@ -26,25 +38,31 @@ label_special <- function (labels, multi_line=FALSE, sep = "== ") {
   out
 }
 
+# verify the break range in brks_vec (FIXME)
+mn <- min(df_random$R0_sub,df_targeted$R0_sub, na.rm = T)
+mx <- max(df_random$R0_sub,df_targeted$R0_sub, na.rm = T)
 
 # show_contours_1, width=24,height=24,message=FALSE,warning=FALSE}
 brks_vec <- seq(0.8,1.05,by=0.05) # Break vector for unifying the legends in Random and TTI testing cases. 
 
-p1 <- ggplot(df2,aes(x=omega,y=rho,z=R0_sub))+ theme_bw() +
+# #################################
+# 1. Plot the Random Testing Scenario:
+# #################################
+df_temp <- df_random
+p1 <- ggplot(df_temp,aes(x=omega,y=rho,z=R0_sub))+ theme_bw() +
   xlab(TeX('$\\omega$, Returning test result rate (1/day)')) +
   ylab(TeX('$\\rho$, testing intensity (1/day per capita)')) +
-  theme(panel.spacing=grid::unit(0,"lines"),legend.position = c(0.89, 0.73)) # initial theme, comment it for TTI testing plot
-# theme(panel.spacing=grid::unit(0,"lines"),legend.position = "none") #AG, uncomment for Random Case
+  # theme(panel.spacing=grid::unit(0,"lines"),legend.position = c(0.89, 0.73)) # initial theme, comment it for TTI testing plot
+  theme(panel.spacing=grid::unit(0,"lines"),legend.position = "none") #AG, uncomment for Random Case
 p1_temp <- (p1
             + geom_contour_filled(breaks=brks_vec)
             + geom_contour(breaks=1,alpha=0.5,colour="black")
-            # + scale_fill_manual(palette=mycolors(brks_vec),drop=FALSE)
             + facet_grid(eta_w~eta_c, labeller=label_special)
             + scale_x_continuous(expand=expansion(c(0,0)), n.breaks=3)
             + scale_y_continuous(expand=expansion(c(0,0)), n.breaks=3)
             + scale_fill_viridis_d(name=parse(text="R[0]"),drop=FALSE)
-            + ggtitle(TeX(sprintf("W_S=%.1f, W_I=W_R=1",params[["W_S"]] )))
-            + geom_rect(data=df2, fill=ifelse(df2$eta_c>df2$eta_w,"grey90","NA" ),
+            + ggtitle(TeX(sprintf("W_S=W_I=W_R=1")))
+            + geom_rect(data=df_temp, fill=ifelse(df_temp$eta_c > df_temp$eta_w,"grey90","NA" ),
                         color= NA,
                         ymin=-1,
                         ymax=10,
@@ -53,9 +71,65 @@ p1_temp <- (p1
 )
 p1_temp
 
-ggsave(p1_temp,filename = "R0contour_TTI.pdf" , path="../pix/")
+ggsave(p1_temp,filename = "R0contour_random.pdf" ,
+       width = 14, height = 14, units = "cm",
+       path="../pix/")
 
 
+# #################################
+# 2. Plot the Random Testing Scenario:
+# #################################
+df_temp <- df_targeted
 
+p2 <- ggplot(df_temp,aes(x=omega,y=rho,z=R0_sub))+ theme_bw() +
+  xlab(TeX('$\\omega$, Returning test result rate (1/day)')) +
+  ylab(TeX('$\\rho$, testing intensity (1/day per capita)')) +
+  theme(panel.spacing=grid::unit(0,"lines"),legend.position = c(0.87, 0.75)) # initial theme, comment it for TTI testing plot
+  
+p2_temp <- (p2
+            + geom_contour_filled(breaks=brks_vec)
+            + geom_contour(breaks=1,alpha=0.5,colour="black")
+            + facet_grid(eta_w~eta_c, labeller=label_special)
+            + scale_x_continuous(expand=expansion(c(0,0)), n.breaks=3)
+            + scale_y_continuous(expand=expansion(c(0,0)), n.breaks=3)
+            + scale_fill_viridis_d(name=parse(text="R[0]"),drop=FALSE)
+            + ggtitle(TeX(sprintf("W_S=%.1f, W_I=W_R=1",W_S_targeted )))
+            + geom_rect(data=df_temp, fill=ifelse(df_temp$eta_c > df_temp$eta_w,"grey90","NA" ),
+                        color= NA,
+                        ymin=-1,
+                        ymax=10,
+                        xmin=-1,
+                        xmax=2)
+)
+p2_temp
 
+ggsave(p2_temp,filename = "R0contour_TTI.pdf" ,
+       width = 14, height = 14, units = "cm",
+       path="../pix/")
 
+# #################################
+# 3. SubPlots the Random Testing Scenario:
+# #################################
+
+df_temp <- df_random %>% dplyr::filter(eta_w==0 & eta_c==0) 
+
+p1 <- ggplot(df_temp,aes(x=omega,y=rho,z=R0_sub))+ theme_bw() +
+  xlab(TeX('$\\omega$, Returning test result rate (1/day)')) +
+  ylab(TeX('$\\rho$, testing intensity (1/day per capita)')) +
+  theme(panel.spacing=grid::unit(0,"lines"),legend.position = "right") 
+p1_temp <- (p1
+            + geom_contour_filled(breaks=brks_vec)
+            + geom_contour(breaks=1,alpha=0.5,colour="black")
+            + facet_grid(eta_w~eta_c, labeller=label_special)
+            + scale_x_continuous(expand=expansion(c(0,0)), n.breaks=3)
+            + scale_y_continuous(expand=expansion(c(0,0)), n.breaks=3)
+            + scale_fill_viridis_d(name=parse(text="R[0]"),drop=FALSE)
+            + ggtitle(TeX(sprintf("W_S=W_I=W_R=1")))
+            + geom_rect(data=df_temp, fill=ifelse(df_temp$eta_c > df_temp$eta_w,"grey90","NA" ),
+                        color= NA,
+                        ymin=-1,
+                        ymax=10,
+                        xmin=-1,
+                        xmax=2)
+)
+p1_temp
